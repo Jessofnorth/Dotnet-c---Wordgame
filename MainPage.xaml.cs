394c80a104510fb176802079ed6c575bfde2ae6b
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-01-06 22:18:32 +0100

    data binding shows the selected word ass _ on screen, methods for random word and checking guessed chars to word is done

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,35 @@
 ﻿using System.ComponentModel;
+using System;
+using System.Diagnostics;
 
 namespace Wordgame;
 
 public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
-    //add the list of words for the game 
+    //
+    #region UI
+    public string Highlight
+    {
+        get => highlight;
+        set
+        {
+            //get/set for the state of the guessed word, updates when the property changes
+            //when a new char is guessed.
+            highlight = value;
+            OnPropertyChanged();
+        }
+    }
+    #endregion
+
+    
     #region Words
+    //the anser property, the word to guess
+    string answer = "";
+
+    //the property for the string with guessed chars and _
+    private string highlight;
+
+    //add the list of words for the game 
     List<string> words = new()
     {
         "lion",
@@ -21,13 +45,45 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
         "penguin",
         "hamster"
     };
+
+    //list with the guessed chars
+    List<char> guesses = new();
     #endregion 
+  
+
 
     public MainPage()
 	{
 		InitializeComponent();
+
+        //Set data binding context to this file
+        BindingContext = this;
+
+        //run the Word picker method for a word to guess
+        WordPicker();
+
+        //run the check guess method to match the answer to the guessed chars
+        CheckGuess(answer, guesses);
 	}
 
+    #region Game
+    private void WordPicker()
+    {
+        //choose a random word in the list and set to answer property
+        answer = words[new Random().Next(0, words.Count)];
+        Debug.WriteLine(answer);
+    }
+
+    private void CheckGuess(string answer, List<char> guessedChars)
+    {
+        //get the random word, answer. Then check all the guessed chars in list, user input, to match word
+        //if a letter in word is not guessed display a _ 
+        var temp = answer.Select(x => (guessedChars.IndexOf(x) >= 0 ? x : '_'))
+            .ToArray();
+        Highlight = string.Join(' ', temp);
+
+    }
+    #endregion
 }
 
 
