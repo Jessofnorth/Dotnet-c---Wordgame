9439323ff8ee28c0d41556475806e1f7df327f72
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-01-06 21:37:07 +0100

    added images for the hangman illustration, added words to list and styled the page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace Wordgame;
+﻿using System.ComponentModel;
 
-public partial class MainPage : ContentPage
+namespace Wordgame;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
-	
+    //add the list of words for the game 
+    #region Words
+    List<string> words = new()
+    {
+        "lion",
+        "hippopotamus",
+        "giraffe",
+        "elephant",
+        "cat",
+        "dog",
+        "moose",
+        "raindeer",
+        "bear",
+        "wolf",
+        "penguin",
+        "hamster"
+    };
+    #endregion 
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
 	}
